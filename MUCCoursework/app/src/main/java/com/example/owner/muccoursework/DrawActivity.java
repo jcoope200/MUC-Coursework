8126390d94c989128b411c2aeee989f86e9d7657
Jamie Cooper <jcoope200@caledonian.ac.uk>
2014-12-21 15:45:28 +0000

    commented DrawActivity

--- MUCCoursework/app/src/main/java/com/example/owner/muccoursework/DrawActivity.java
+++ MUCCoursework/app/src/main/java/com/example/owner/muccoursework/DrawActivity.java
@@ -15,6 +15,9 @@ public class DrawActivity extends Activity{
     @TargetApi(Build.VERSION_CODES.HONEYCOMB)
     @Override
     protected void onCreate(Bundle savedInstanceState){
+        //initialises the XML view draw_screen to the content view associated with the activity
+        //forces a change to landscape orientation
+        //passes the context to a new instance of the DrawingSurfaceView class
         super.onCreate(savedInstanceState);
         setContentView(R.layout.draw_screen);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
