535ba12e04f31521b0908239cda9c20aae1a3fde
Jamie Cooper <jcoope200@caledonian.ac.uk>
2014-12-21 17:02:49 +0000

    commented MainActivity

--- MUCCoursework/app/src/main/java/com/example/owner/muccoursework/MainActivity.java
+++ MUCCoursework/app/src/main/java/com/example/owner/muccoursework/MainActivity.java
@@ -36,6 +36,12 @@ public class MainActivity extends Activity{
 
     @Override
     public void onCreate(Bundle savedInstanceState){
+        //constrain application to portrait orientation and attach the main_activity.xml file to the view
+        //call the methods to add the races to the second spinner
+        //give each item on the spinners a listener
+        //get the default preferences for the spinner and store it as a variable
+        //call fragment manager to set the About dialogue
+        //create an instance of DatabaseInfo to handle the information of selected driver
         super.onCreate(savedInstanceState);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         setContentView(R.layout.activity_main);
@@ -52,6 +58,8 @@ public class MainActivity extends Activity{
     }
 
     public void addItemsOnRaceSpinner(){
+        //populate race spinner with the races in an array list
+        //set up an array adapter to use these values from a drop down value
         raceSpin = (Spinner) findViewById(R.id.raceSpin);
         List<String> listRace = new ArrayList<String>();
         listRace.add("Australia");
@@ -79,11 +87,13 @@ public class MainActivity extends Activity{
     }
 
     public void addListenerOnSelectedItem(){
+        //give each driver a listener to handle events when they are chosen
         driverSpin = (Spinner) findViewById(R.id.driverSpin);
         driverSpin.setOnItemSelectedListener(new CustomOnItemSelectedListener());
     }
 
     public void addListener(){
+        //create objects of Driver and Summary based on the selected driver
         driverSpin = (Spinner) findViewById(R.id.driverSpin);
         final String driverString = driverSpin.getSelectedItem().toString();
         raceSpin = (Spinner) findViewById(R.id.raceSpin);
@@ -98,6 +108,11 @@ public class MainActivity extends Activity{
         btnSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                //show a toast message with the chosen Driver and Race
+                //determine the proper summary and link the mainactivity to DriverPage using an intent
+                //create an instance of the FormulaOneDatabaseManager to handle database interactions
+                //find the proper driver data based on the selected item on the spinner
+                //put forward the database information and the RSS feed to the new intent and start DriverPage
                 Toast.makeText(MainActivity.this, "Driver: "+String.valueOf(driverSpin.getSelectedItem())
                 + "\nRace: " + String.valueOf(raceSpin.getSelectedItem()), Toast.LENGTH_SHORT).show();
                 selectedDriverSum.determineDriverSummary(driverSpin.getSelectedItemPosition(), driverSpin.getSelectedItemPosition()+1);
@@ -129,6 +144,7 @@ public class MainActivity extends Activity{
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu){
+        //create the menu across the top of the app's screen
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu, menu);
         return true;
@@ -136,6 +152,10 @@ public class MainActivity extends Activity{
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item){
+        //start different activities based on the ID of the selected menu item
+        //Map and Draw start their corresponding activities
+        //About creates the DialogFragment and displays it
+        //Quit closes the app
         switch(item.getItemId()){
             case R.id.Map:
                 Intent mapHere = new Intent(this, MapActivity.class);
